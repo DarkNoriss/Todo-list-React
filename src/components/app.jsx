94d17e4d376443723b5d27ef858fb6fc21b77549
DarkNoriss <darknoriss@gmail.com>
2023-03-29 05:33:27 +0200

    working on tasks

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Modal } from "./modal";
 import "../styles/app.scss";
 
 export const App = () => {
-  return <div className="app"></div>;
+  // const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState([]);
+  // const taskDescRef = useRef();
+
+  function handleAddTask(event) {
+    console.log(event);
+    setNewTask({ name: `${event.name}`, desc: `${event.desc} ` });
+    console.log({ newTask });
+  }
+
+  const showModal = () => {
+    const modal = document.querySelector(".modal");
+    modal.showModal();
+  };
+
+  return (
+    <div className="app">
+      <nav className="nav-bar">
+        <button onClick={showModal}>Create Task</button>
+        <button>Remove finished tasks</button>
+      </nav>
+      <div className="tasks-container"></div>
+      <Modal onCreateTask={handleAddTask} />
+    </div>
+  );
 };
