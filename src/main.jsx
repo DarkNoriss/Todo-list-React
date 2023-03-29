28f1992473d8a4dd7717b5c6cc3d6883f0e47799
DarkNoriss <darknoriss@gmail.com>
2023-03-29 05:34:40 +0200

    moving modal to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { App } from "./components/app";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
-import { Modal } from "./components/modal";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
@@ -11,6 +10,5 @@ ReactDOM.createRoot(document.querySelector("#root")).render(
     <Header />
     <App />
     <Footer />
-    <Modal />
   </React.StrictMode>
 );
