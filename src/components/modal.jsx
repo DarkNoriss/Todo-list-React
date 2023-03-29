45c59c0d37fbcfd6e81372f16034a2d30ad7d01b
DarkNoriss <darknoriss@gmail.com>
2023-03-29 05:34:59 +0200

    adding handleSubmit to modal form

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "../styles/modal.scss";
 
-export const Modal = () => {
+export const Modal = ({ onCreateTask }) => {
+  const handleSubmit = (event) => {
+    const form = event.target;
+    const name = form.elements.name.value;
+    const desc = form.elements.desc.value;
+
+    form.reset();
+    onCreateTask({ name, desc });
+  };
   return (
-    <dialog>
-      <form>
-        <p></p>
-        <input></input>
-        <textarea></textarea>
-        <button></button>
+    <dialog className="modal">
+      <form method="dialog" onSubmit={handleSubmit}>
+        <p>Creating task</p>
+        <input placeholder="Name" name="name" type="text" required />
+        <textarea placeholder="Description" name="desc" rows="4" cols="50" />
+        <button>Create</button>
       </form>
     </dialog>
   );
