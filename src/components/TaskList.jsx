3abb8f121881d6d0161aaa6cf52ed9e0073fea80
DarkNoriss <darknoriss@gmail.com>
2023-03-30 05:58:36 +0200

    moving Task to TaskList

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "../styles/task.scss";
+
+export const TaskList = ({ tasks, handleTaskDone }) => {
+  const createTask = tasks.map((task) => {
+    return <Task key={task.id} task={task} handleTaskDone={handleTaskDone} />;
+  });
+
+  return <> {createTask}</>;
+};
+
+const Task = ({ task, handleTaskDone }) => {
+  const [checked, setChecked] = useState(() => task.done);
+  const [init, setInit] = useState(() => true);
+
+  useEffect(() => {
+    if (init) return setInit(!init);
+    handleTaskDone(task.id, checked);
+  }, [checked]);
+
+  return (
+    <div className="task" data-task-id={task.id}>
+      <h2>{task.name}</h2>
+      <p>{task.desc}</p>
+      <div className="done">
+        <input
+          type="checkbox"
+          id="checkbox"
+          className="done-checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+        <label className="task-done" htmlFor="checkbox">
+          Done
+        </label>
+      </div>
+    </div>
+  );
+};
