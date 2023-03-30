53afd8a4275fe243f924f574f155359c8e73daf3
DarkNoriss <darknoriss@gmail.com>
2023-03-30 05:58:47 +0200

    securing the loadData

--- src/utils/localStorageData.jsx
+++ src/utils/localStorageData.jsx
@@ -1,10 +1,17 @@
 import { LOCAL_STORAGE_KEY } from "../constants";
 
 export const loadData = () => {
-  const storedTasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-  return storedTasks;
+  console.log("Loading local storage...");
+  try {
+    const storedTasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    return storedTasks || [];
+  } catch (error) {
+    console.error("Error loading data from local storage:", error.message);
+    return [];
+  }
 };
 
 export const saveData = (array) => {
+  console.log("Saving to local storage...");
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(array));
 };
