a6f8ee851600b4a05e7769b20a5b33a5a586b501
DarkNoriss <darknoriss@gmail.com>
2023-03-30 05:56:23 +0200

    moving header and footer to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/app";
-import { Footer } from "./components/footer";
-import { Header } from "./components/header";
+import { App } from "./components/App";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
-    <Header />
     <App />
-    <Footer />
   </React.StrictMode>
 );
